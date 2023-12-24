d6c634974086d7cd8ad840e1f7dc6bf6cc2bb153
Alex-Nav <alejugger@gmail.com>
2023-12-23 23:00:49 -0600

    changes

--- Portafolio1/src/components/about/About.jsx
+++ Portafolio1/src/components/about/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './about.css'
+import cdmx from '../../assets/cdmx.jpg'
 const about = () => {
     return (
         <div className='a'>
-            <div className="a-left">lef</div>
+            <div className="a-left">
+                <div className="a-card a-bg"></div>
+                <div className="a-card">
+                    <img src={cdmx}alt="" />
+                </div>
+            </div>
             <div className="a-right">rig</div>
         </div>
     )
