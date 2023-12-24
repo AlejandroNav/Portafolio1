96aa96482ac3f2ffb7ea6434836068add4c41a71
Alex-Nav <alejugger@gmail.com>
2023-12-23 19:29:57 -0600

    start 2nd section

--- Portafolio1/src/components/intro/Intro.jsx
+++ Portafolio1/src/components/intro/Intro.jsx
@@ -10,16 +10,18 @@ const Intro = () => {
                     <h1 className='i-name'>Alex Nava</h1>
                     <div className='i-title'>
                         <div className="i-title-wrapper">
+                            <div className='i-title-item'>Software Engineer</div>
                             <div className='i-title-item'>Web Developer</div>
-                            <div className='i-title-item'>Disenador UI/UX</div>
-                            <div className='i-title-item'>Escritor tecnico</div>
+                            <div className='i-title-item'>Diseñador UI/UX</div>
+                            <div className='i-title-item'>Traductor Profesional</div>
+                            <div className='i-title-item'>Escritor Técnico</div>
                             <div className='i-title-item'>Creador de Contenido</div>
-                            <div className='i-title-item'>Especialista de Accesibilidad</div>
+                            <div className='i-title-item'>Especialista en Accesibilidad</div>
                         </div>
                     </div>
                     <div className='i-desc'>
                         Soy un ingeniero de software mexicano y especialista en accesibilidad,
-                        me encanta mezclar el arte del diseño con el tecnicismo de la informática
+                        me encanta mezclar el arte del diseño con el tecnicismo de la informática.
                     </div>
                 </div>
             </article>
