ea0303e9f4eccdfcfc1acb2c2717218483197487
Alex-Nav <alejugger@gmail.com>
2024-01-08 22:19:30 -0600

    cambio

--- Portafolio1/src/components/productList/ProductList.jsx
+++ Portafolio1/src/components/productList/ProductList.jsx
@@ -4,11 +4,11 @@ import Product from '../product/Product'
 const ProductList = () => {
   return (
     <section className='list'>
-      <article  className='list-texts'>
+      <article className='list-texts'>
         <h2 className="list-title">Crear e Inspirar</h2>
-        <p className="list-description">Portafolio creativo</p>
+        <p className="list-description">¿Buscas elevar tu negocio en el mundo digital? Estás en el lugar correcto. Nos especializamos en el desarrollo web personalizado, desde Landing Pages hasta manejo completo del diseño y presencia digital de marca</p>
       </article>
-      <Product/><Product/><Product/>
+      <Product /><Product /><Product />
     </section>
   )
 }
