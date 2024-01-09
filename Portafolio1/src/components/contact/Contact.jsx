03355e5a3eb831fac3f889c2db18456ec9b602f2
Alex-Nav <alejugger@gmail.com>
2024-01-08 23:39:22 -0600

    a mimir

--- Portafolio1/src/components/contact/Contact.jsx
+++ Portafolio1/src/components/contact/Contact.jsx
@@ -3,7 +3,22 @@ import './contact.css'
 const Contact = () => {
     return (
         <section className='contact'>
+            <div className="contact-bg">
+                <div className='contact-wrapper'>
+                    <article className='contact-left'>
+                        <h1 className="contact-title">Discutamos tu projecto</h1>
+                        <div className="contact-info">
+                            <div className='contact-item'>
+                                <img src="" alt="" />
+                                img
+                            </div>
+                        </div>
+                    </article>
+                    <article className='contact-right'>
 
+                    </article>
+                </div>
+            </div>
         </section>
     )
 }
