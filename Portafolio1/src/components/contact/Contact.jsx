ff870050950063b17c79e5beb1e530503b1482e2
Alex-Nav <alejugger@gmail.com>
2024-01-14 13:11:56 -0600

    form

--- Portafolio1/src/components/contact/Contact.jsx
+++ Portafolio1/src/components/contact/Contact.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { Icon } from '@iconify/react';
 const Contact = () => {
+    const formRef = useRef()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const form = formRef.current;
+
+        try {
+            await fetch('/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: new URLSearchParams(new FormData(form)).toString(),
+            });
+            // Handle success, e.g., show a success message or redirect
+            console.log('Form submitted successfully!');
+        } catch (error) {
+            // Handle error, e.g., show an error message
+            console.error('Error submitting the form:', error);
+        }
+    };
+
+
     return (
         <section className='contact'>
             <div className="contact-bg">
@@ -26,17 +47,15 @@ const Contact = () => {
                 </article>
                 <article className='contact-right'>
                     <p className="contact-desc">¡Haz realidad tu visión digital hoy! Con nuestro equipo en desarrollo web, transformaremos tus ideas en experiencias impactantes. ¡Eleva tu presencia en línea y alcanza nuevos horizontes! Contáctanos para empezar.</p>
-                    <form action="">
+                    <form ref={formRef} onSubmit={handleSubmit} data-netlify="true">
                         <input type="text" placeholder='Nombre' name='user_name' />
                         <input type="text" placeholder='Tema' name='user_subject' />
-                        <input type="text" placeholder='Usuario' name='user_email' />
-                        <textarea name="user_message" id="" cols="30" rows="5" placeholder='mensaje'></textarea>
-                        <button type="submit">Enviar</button>
+                        <input type="text" placeholder='Correo' name='user_email' />
+                        <textarea name="user_message" id="" cols="30" rows="5" placeholder='Escribe tu Mensaje'></textarea>
+                        <button type="submit">Enviar Mensaje</button>
                     </form>
                 </article>
-
             </div>
-
         </section>
     )
 }
